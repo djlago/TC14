b0cf7419adf56b4544040ff52a8e0fe46fc56859
Daniel Lagomarsino <daniel.lagomarsino@smartbear.com>
2021-07-15 12:10:18 -0400

    7/15/21

---
+++ TC14_8/Script/OCR_Chart.py
@@ -0,0 +1,12 @@
+﻿def OCR_Chart():
+    #Check whether '*Fresh Vegetables*' matches the text optically recognized in the image Aliases.browser.pageConsumerSalesSalesAnalysisSh.article.canvas.
+    OCR.Recognize(Aliases.browser.pageConsumerSalesSalesAnalysisSh.article.canvas).CheckText("*Fresh Vegetables*")
+    #Check whether '*Fresh Fruit*' matches the text optically recognized in the image Aliases.browser.pageConsumerSalesSalesAnalysisSh.article.canvas.
+    OCR.Recognize(Aliases.browser.pageConsumerSalesSalesAnalysisSh.article.canvas).CheckText("*Fresh Fruit*")
+    #Simulates a user action over the area that contains the recognized text.
+    OCR.Recognize(Aliases.browser.pageConsumerSalesSalesAnalysisSh.article.canvas).BlockByText("Fresh Vegetables").Click()
+    OCR.Recognize(Aliases.browser.pageConsumerSalesSalesAnalysisSh.article.canvas).CheckText("*Cheese*")
+
+
+
+OCR.Recognize()
